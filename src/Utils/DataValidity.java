1c697a13a0e44dbccf01af0b6d88bedc53da1c14
Simonas Bazinys <sbazinys@gmail.com>
2021-03-28 18:04:20 +0300

    phone number verification and unit tests

--- src/Utils/DataValidity.java
+++ src/Utils/DataValidity.java
@@ -1,5 +1,8 @@
 package Utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DataValidity {
     public static boolean isValidEmailAddress(String email)
     {
@@ -18,6 +21,19 @@ public class DataValidity {
     {
 
         return password.length()<=15 && password.length() != 0;
+    }
+
+    public static boolean isValidLithuanianPhoneNumber(String phoneNumber) {
+
+
+        // Compile regular expression
+        Pattern pattern = Pattern.compile("^\\+3706\\d\\d\\d\\d\\d\\d\\d$" ,Pattern.CASE_INSENSITIVE);
+        Pattern alternatePattern = Pattern.compile("^86\\d\\d\\d\\d\\d\\d\\d$" ,Pattern.CASE_INSENSITIVE);
+        // Match regex against phoneNumber
+        Matcher matcher = pattern.matcher(phoneNumber);
+        Matcher alternateMatcher = alternatePattern.matcher(phoneNumber);
+        // Use results...
+        return matcher.matches() || alternateMatcher.matches();
     }
 
 
