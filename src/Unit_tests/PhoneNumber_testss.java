8c8c701f2d839f505d5999430718171496403940
Tautvydas <tautvydas.kvietkauskas@gmail.com>
2021-03-28 18:13:27 +0300

    Number validation

--- src/Unit_tests/PhoneNumber_testss.java
+++ src/Unit_tests/PhoneNumber_testss.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class PhoneNumber_tests {
+public class PhoneNumber_testss {
         @Test
         public void numberWithLessThan15symbolShouldBeCorrect()
         {
