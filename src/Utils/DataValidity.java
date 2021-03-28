b714a21a1d7f752974c1daf67a21311d983a4b74
Tautvydas <tautvydas.kvietkauskas@gmail.com>
2021-03-28 17:36:40 +0300

    Phone number test

--- src/Utils/DataValidity.java
+++ src/Utils/DataValidity.java
@@ -16,8 +16,17 @@ public class DataValidity {
 
     public static boolean isValidPasswordLength(String password)
     {
-
-        return password.length()<=15 && password.length() != 0;
+        return password.length()<=15 && password.length() > 0;
+    }
+    public static boolean isValidNumber(String phoneNumber)
+    {
+        return phoneNumber.length()<=15 && phoneNumber.length() > 5;
+    }
+    public static boolean isValidNumberStartWithPlus(String phoneNumber){
+        String PhonePattern = "^[+][0-9]+";
+        java.util.regex.Pattern pp = java.util.regex.Pattern.compile(PhonePattern);
+        java.util.regex.Matcher mm = pp.matcher(phoneNumber);
+        return mm.matches();
     }
 
 
